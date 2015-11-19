2b0ecf3021d3adea378d89e538796aee79e8105b
tracehelms <tracehelms@gmail.com>
2015-11-19 15:42:19 -0700

    get issues, sort, and take the first n items

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,22 @@
+defmodule Issues.GithubIssues do
+  @user_agent [ {"User-agent", "Elixir test"}]
+  @github_url Application.get_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response(%{status_code: 200, body: body}) do
+    {:ok, Poison.decode!(body)}
+  end
+  def handle_response(%{status_code: ___, body: body}) do
+    {:error, Poison.decode!(body)}
+  end
+
+end
